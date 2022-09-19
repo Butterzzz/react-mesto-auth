85f0ed1043d9975737f6c6b0974b2d13a5247588
Grinchenko Sergei <butters.blaga@gmail.com>
2022-09-20 05:48:06 +0900

    feat: add navigation  markup to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { useLocation, Link } from "react-router-dom";
 import logo from '../images/header-logo.svg';
+import './styles/Header.css';
 
-function Header() {
+function Header({ email, signOut }) {
+
+  const location = useLocation();
+
   return (
     <header className="header">
-      <img className="header__logo" src={logo} alt="Логотип сервиса Mesto" />
-    </header>
+      <a className="header__logo" href="/">
+        <img className="header__logo" src={logo} alt="Логотип сервиса Mesto" />
+      </a>
+
+      {location.pathname === "/" && (
+        <div className="header__container">
+          <p className="header__email">{email}email@mail.com</p>
+          <button className="header__button" onClick={signOut}>Выйти</button>
+        </div>
+      )}
+
+      {location.pathname === "/sign-up" && (
+        <div className="header__container">
+          <Link to="/sign-in" className="header__link">Войти</Link>
+        </div>
+      )}
+
+      {location.pathname === "/sign-in" && (
+        <div className="header__container">
+          <Link to="/sign-up" className="header__link">Регистрация</Link>
+        </div>
+      )}
+
+    </header >
   );
 }
+
 export default Header;
